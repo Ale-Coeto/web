3eb649efe58ae3c9595decd174aeeb914c6423af
Alejandra Coeto <a01285221@tec.mx>
2024-05-19 23:21:49 -0600

    forms practice

---
+++ SuperheroName/SuperheroName/Pages/Index.cshtml.cs
@@ -0,0 +1,138 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using System.Globalization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Diagnostics.Metrics;
+
+namespace SuperheroName.Pages;
+
+public class IndexModel : PageModel
+{
+    [BindProperty]
+    public string Name { set; get; } = "";
+
+    [BindProperty]
+    public int Month { set; get; } = -1;
+
+    [BindProperty]
+    public int Season { set; get; } = -1;
+
+    [BindProperty]
+    public string Date { set; get; } = "";
+
+    private List<string> letterNames = new List<string>
+    {
+        "Captain", "Wonder", "Super", "Phantom", "Dark", "Incredible", "Professor", "Iron", "Hawk", "Archer", "Steel", "Bolt",
+        "Atomic", "Torch", "Space", "Mega", "Turbo", "Fantastic", "Invisible", "Night", "Silver", "Aqua", "Amazing", "Giant", "Rock", "Power"
+    };
+
+    public List<string>[] seasonMonthNames { get; } = new List<string>[] {
+        new List<string> { "Bloom", "Blossom", "Raindrop", "Tulip", "Petal", "Fresh", "Daffodil", "Sprout", "Falcon", "Umbrella", "Butterfly", "Garden" },
+        new List<string> { "Sunshine", "Sea", "Heatwave", "Tropical", "Sandcastle", "Seashell", "Palm", "Wave", "Flare", "Heat", "Hot", "Shark" },
+        new List<string> { "Snowflake", "Freeze", "Blizzard", "Iceberg", "Frost", "Snowman", "Glacier", "Polar", "Blue", "Snow", "Ice", "Evergreen" },
+        new List<string> { "Harvest", "Maple", "Wind", "Haystack", "Thunder", "Rustic", "Cider", "Cozy", "Leaves", "Eagle", "Ninja", "Cider" }
+    };
+
+
+    public string HeroName { set; get; } = "";
+    public string Message { set; get; } = "";
+    public int bday { set; get; }
+    public int bmonth { set; get; }
+    public int byear { set; get; }
+
+    public bool correctName { set; get; } = true;
+    public bool correctSeason { set; get; } = true;
+    public bool correctMonth { set; get; } = true;
+    public bool correctBirthday { set; get; } = true;
+    public bool noBirthday { set; get; } = false;
+
+    private void resetVars()
+    {
+        correctName = true;
+        correctMonth = true;
+        correctBirthday = true;
+        noBirthday = false;
+    }
+
+    private string getName()
+    {
+        string hero = "";
+        char firstLetter = Name[0];
+        int value = char.ToLower(firstLetter) - 'a';
+
+        hero += letterNames[value];
+        hero += " ";
+        hero += seasonMonthNames[Season][Month];
+
+        return hero;
+    }
+    public void OnGet()
+    {
+
+    }
+
+    public void OnPost()
+    {
+        resetVars();
+        
+
+        if (Date == null || Date == "")
+        {
+            correctBirthday = false;
+            noBirthday = true;
+            Message = "Please select or write your birthday!";
+        } else
+        {
+            try
+            {
+                DateTime date = DateTime.ParseExact(Date, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                if (date > DateTime.Now)
+                {
+                    correctBirthday = false;
+                    Message = "Please select a valid birthday!";
+                }
+
+
+                bmonth = date.Month;
+                bday = date.Day;
+                byear = date.Year;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Message = "Invalid birthday";
+                correctBirthday = false;
+            }
+        }
+
+        
+        
+        if (Month == -1)
+        {
+            correctMonth = false;
+            Message = "Please select your favorite month!";
+        }
+
+        if (Season == -1)
+        {
+            correctSeason = false;
+            Message = "Please select your favorite season!";
+        }
+
+        if (Name == "" || Name == null)
+        {
+            correctName = false;
+            Message = "Please write your name!";
+        }
+
+
+        if (correctName && correctSeason && correctMonth && correctBirthday)
+        {
+            HeroName = getName();
+            Response.Redirect($"Result?name={HeroName}&month={bmonth}&season={Season}");
+        }
+    }
+}
+
---
+++ SuperheroName/SuperheroName/Pages/Result.cshtml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SuperheroName.Pages
+{
+	public class ResultModel : PageModel
+    {
+        public string HeroName { set; get; } = "";
+        public string Month { set; get; } = "";
+        public string MonthHero { set; get; } = "";
+        public string ImgPath { set; get; } = "";
+        public string Description { set; get; } = "";
+
+        public static List<string> months = new List<string> {
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+        };
+
+        public static List<string> descriptions = new List<string> {
+            "Your hero represents the promise of new beginnings and fresh starts, embracing the beauty of nature's rebirth.",
+            "Your hero radiates warmth and energy, embracing the joy of sunny days and outdoor adventures.",
+            "Your hero embodies resilience and fortitude, standing strong in the face of adversity like a towering mountain in a snowstorm.",
+            "Your hero is a beacon of hope and renewal, embracing change and growth like the vibrant colors of autumn leaves."
+        };
+
+
+
+        List<string> superheroesByMonth = new List<string>
+        {
+            "The Flash (Barry Allen)",       // January
+            "Black Panther (T'Challa)",      // February
+            "Green Arrow (Oliver Queen)",    // March
+            "Captain America (Steve Rogers)",// April
+            "Iron Man (Tony Stark)",         // May
+            "Batman (Bruce Wayne)",          // June
+            "Spider-Man (Peter Parker)",     // July
+            "Superman (Clark Kent/Kal-El)",  // August
+            "Hawkeye (Clint Barton)",        // September
+            "Nightwing (Dick Grayson)",      // October
+            "Hulk (Bruce Banner)",           // November
+            "Deadpool (Wade Wilson)"         // December
+        };
+
+        public void OnGet()
+        {
+            int monthValue = 0;
+            int seasonValue = 0;
+
+            if (Request.Query.ContainsKey("name"))
+            {
+                HeroName = Request.Query["name"].ToString() ?? "";
+            }
+
+            if (Request.Query.ContainsKey("month"))
+            {
+                int.TryParse(Request.Query["month"], out monthValue);
+            }
+
+            if (Request.Query.ContainsKey("season"))
+            {
+                int.TryParse(Request.Query["season"], out seasonValue);
+            }
+
+            Month = months[monthValue-1];
+            MonthHero = superheroesByMonth[monthValue-1];
+            Description = descriptions[seasonValue];
+            ImgPath = "/images/" + monthValue + ".jpeg";
+        }
+    }
+}
